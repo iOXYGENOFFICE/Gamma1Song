8dec011f1d450cabe0a7b259bd868e9394b798e0
iOXYGENOFFICE <116354269+iOXYGENOFFICE@users.noreply.github.com>
2022-10-29 21:55:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const chalk = require('chalk');
+
+console.log(`[${chalk.magenta('GAMMA1')}] Ente kadang-kadang ..`)
+
+setTimeout(() => {
+  console.clear();
+  console.log(`[${chalk.magenta('GAMMA1')}] Ini bukan cerita cinta ${chalk.bold('satu')} atau ${chalk.bold('dua')}`)
+}, 1000)
+
+setTimeout(() => {
+  console.clear();
+  console.log(`[${chalk.magenta('GAMMA1')}] Yang membuat aku harus bingung`)
+}, 6000)
+
+setTimeout(() => {
+  console.clear();
+  console.log(`[${chalk.magenta('GAMMA1')}] Pilih-pilih kamu atau dia`)
+}, 8000)
+
+setTimeout(() => {
+  console.clear();
+  console.log(`[${chalk.magenta('GAMMA1')}] Bukan satu atau dua`)
+}, 10000)
+
+// setTimeout(() => {
+//   console.log(`[${chalk.magenta('GAMMA1')}] Ku berharap kau percaya`)
+// }, 11000)
+//
+// setTimeout(() => {
+//   console.log(`[${chalk.magenta('GAMMA1')}] Cintaku padamu tulus apa adanya`)
+// }, 13000)
